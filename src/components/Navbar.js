0fff1498fd900eac8a35972c52df732eb4466ca5
Sarthak Kumar <famousfive.17@gmail.com>
2024-03-03 12:08:16 +0530

    100%?

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -6,7 +6,7 @@ export class Navbar extends Component {
       <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark" >
           <div className="container-fluid">
-            <a className="navbar-brand" href="#">NewsChimp</a>
+            <a className="navbar-brand">NewsChimp</a>
             <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
               <span className="navbar-toggler-icon"></span>
             </button>
